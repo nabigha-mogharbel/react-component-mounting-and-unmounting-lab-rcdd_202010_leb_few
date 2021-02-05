e4ea1c9fc5535b78eccab65e8be3063c76a8e8b5
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2021-02-05 15:23:31 +0000

    Automatically backed up by Learn

--- src/components/Game.js
+++ src/components/Game.js
@@ -18,7 +18,7 @@ class Game extends React.Component {
 
   // TODO: create a componentDidMount() which will set the current time
   componentDidMount(){
-    thia.setState({time: setCurrentTime})
+    this.setState({time: setCurrentTime})
   }
   
   setCurrentTime = () => {
